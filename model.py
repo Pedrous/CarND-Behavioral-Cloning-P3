b4dc2d66edbc6222f22c71dc6d2edda809208673
Petri Manninen <manninenpetri84@gmail.com>
2017-07-17 15:43:46 +0300

    explanations now in the model.py

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import os
 import csv
 
+# Open the new data that I have collected
 samples = []
 with open('./newdata/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
@@ -16,6 +17,7 @@ import cv2
 import numpy as np
 import sklearn
 
+# Define a generator to save memory
 def generator(samples, batch_size=32):
     num_samples = len(samples)
     while 1: # Loop forever so the generator never terminates
@@ -27,14 +29,17 @@ def generator(samples, batch_size=32):
             angles = []
             for batch_sample in batch_samples:
             	for i in range(3):
+            		# Define the image path from the .csv lines and load the image
 		            name = './newdata/IMG/'+batch_sample[i].split('/')[-1]
 		            center_image = cv2.imread(name)
+		            # add or reduce the steering angle if it is a side camera image
 		            if i == 1:
 		            	center_angle = float(batch_sample[3]) + 0.2
 		            if i == 2:
 		            	center_angle = float(batch_sample[3]) - 0.2
 		            else:
 		            	center_angle = float(batch_sample[3])
+		            # Extend the images and angles to be returned for the training
 		            images.extend([center_image, cv2.flip(center_image,1)])
 		            angles.extend([center_angle, -center_angle])
 
@@ -49,38 +54,53 @@ validation_generator = generator(validation_samples, batch_size=32)
 
 ch, row, col = 3, 160, 320  # Trimmed image format
 
-# Initial Setup for Keras
+# import the needed Keras tools
 from keras.models import Sequential
 from keras.layers import Cropping2D
 from keras.layers.core import Lambda, Dense, Activation, Flatten, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
+### THE MODEL ###
 model = Sequential()
-# Preprocess incoming data, centered around zero with small standard deviation 
+# Preprocess incoming data, centered around zero with small standard deviation and the data is cropped
 model.add(Lambda(lambda x: x/255 - 0.5, input_shape=(row, col, ch)))#, output_shape=(ch, row, col)))
-model.add(Cropping2D(cropping=((70,25), (0,0)), input_shape = (row, col, ch)))
+model.add(Cropping2D(cropping=((70,25), (0,0))))#, input_shape = (row, col, ch)))
+
+# First convolutional layer with dropout and activation
 model.add(Convolution2D(24,5,5, subsample=(2,2)))
 #model.add(MaxPooling2D())
 model.add(Dropout(0.2))
 model.add(Activation('relu'))
+
+# Second convolutional layer with dropout and activation
 model.add(Convolution2D(36,5,5, subsample=(2,2)))
 #model.add(MaxPooling2D())
 model.add(Dropout(0.2))
 model.add(Activation('relu'))
+
+# Third convolutional layer with dropout and activation
 model.add(Convolution2D(48,5,5, subsample=(2,2)))
 #model.add(MaxPooling2D())
 model.add(Dropout(0.2))
 model.add(Activation('relu'))
+
+# Fourth convolutional layer with dropout and activation
 model.add(Convolution2D(64,3,3,activation="relu"))
 #model.add(MaxPooling2D())
-model.add(Dropout(0.52))
+model.add(Dropout(0.2))
 model.add(Activation('relu'))
+
+# Fifth convolutional layer with dropout and activation
 model.add(Convolution2D(64,3,3))
 #model.add(MaxPooling2D())
-model.add(Dropout(0.5))
+model.add(Dropout(0.2))
 model.add(Activation('relu'))
+
+# Flattening
 model.add(Flatten())#input_shape = (row, col, ch)))
+
+# Four connected layers with activations
 model.add(Dense(1164))
 model.add(Activation('relu'))
 model.add(Dense(100))
@@ -91,9 +111,11 @@ model.add(Dense(10))
 model.add(Activation('relu'))
 model.add(Dense(1))
 
+# Training the model
 model.compile(loss='mse', optimizer='adam')
 model.fit_generator(train_generator, samples_per_epoch= \
             6*len(train_samples), validation_data=validation_generator, \
             nb_val_samples=6*len(validation_samples), nb_epoch=10)
-            
+
+# Save the model   
 model.save('model.h5')
