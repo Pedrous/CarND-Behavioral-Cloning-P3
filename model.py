acd948c5f551311242e9c34aa2fccea0ab9e1b32
Petri Manninen <manninenpetri84@gmail.com>
2017-07-18 17:39:44 +0300

    uusi ajo

--- model.py
+++ model.py
@@ -55,11 +55,12 @@ validation_generator = generator(validation_samples, batch_size=32)
 ch, row, col = 3, 160, 320  # Trimmed image format
 
 # import the needed Keras tools
-from keras.models import Sequential
+from keras.models import Sequential, Model
 from keras.layers import Cropping2D
 from keras.layers.core import Lambda, Dense, Activation, Flatten, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
+import matplotlib.pyplot as plt
 
 ### THE MODEL ###
 model = Sequential()
@@ -70,52 +71,63 @@ model.add(Cropping2D(cropping=((70,25), (0,0))))#, input_shape = (row, col, ch))
 # First convolutional layer with dropout and activation
 model.add(Convolution2D(24,5,5, subsample=(2,2)))
 #model.add(MaxPooling2D())
-model.add(Dropout(0.2))
-model.add(Activation('relu'))
+model.add(Activation('elu'))
 
 # Second convolutional layer with dropout and activation
 model.add(Convolution2D(36,5,5, subsample=(2,2)))
 #model.add(MaxPooling2D())
-model.add(Dropout(0.2))
-model.add(Activation('relu'))
+model.add(Activation('elu'))
 
 # Third convolutional layer with dropout and activation
 model.add(Convolution2D(48,5,5, subsample=(2,2)))
 #model.add(MaxPooling2D())
-model.add(Dropout(0.2))
-model.add(Activation('relu'))
+model.add(Activation('elu'))
 
 # Fourth convolutional layer with dropout and activation
-model.add(Convolution2D(64,3,3,activation="relu"))
+model.add(Convolution2D(64,3,3))
 #model.add(MaxPooling2D())
-model.add(Dropout(0.2))
-model.add(Activation('relu'))
+model.add(Activation('elu'))
 
 # Fifth convolutional layer with dropout and activation
 model.add(Convolution2D(64,3,3))
 #model.add(MaxPooling2D())
-model.add(Dropout(0.2))
-model.add(Activation('relu'))
+model.add(Activation('elu'))
 
 # Flattening
 model.add(Flatten())#input_shape = (row, col, ch)))
 
 # Four connected layers with activations
 model.add(Dense(1164))
-model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Activation('elu'))
 model.add(Dense(100))
-model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Activation('elu'))
 model.add(Dense(50))
-model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Activation('elu'))
 model.add(Dense(10))
-model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Activation('elu'))
 model.add(Dense(1))
 
 # Training the model
 model.compile(loss='mse', optimizer='adam')
-model.fit_generator(train_generator, samples_per_epoch= \
+history_object = model.fit_generator(train_generator, samples_per_epoch= \
             6*len(train_samples), validation_data=validation_generator, \
-            nb_val_samples=6*len(validation_samples), nb_epoch=10)
+            nb_val_samples=6*len(validation_samples), nb_epoch=10, verbose = 1)
+
+### print the keys contained in the history object            
+print(history_object.keys())
+
+### plot the training and validation loss for each epoch
+plt.plot(history_object.history['loss'])
+plt.plot(history_object.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.show()
 
 # Save the model   
 model.save('model.h5')
